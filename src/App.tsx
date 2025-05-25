c8325b7371557eda15a46d8363b6781af0207e5d
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-25 16:15:40 +0500

    used types for children and props in the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/Header";
+
+import GoldImg from "./assets/goals.jpg";
 
 export default function App() {
   return (
     <main>
+      <Header img={{ src: GoldImg, alt: "image of goal" }}>
+        <h1>Your first goal</h1>
+      </Header>
       <CourseGoal
         title="Learn Typescript with React"
         description="Deep dive in Typescript with react"
