039763a84dd07148b6e5ba9a075b31f62ced126c
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-25 15:54:01 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CourseGoal from "./components/CourseGoal";
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal
+        title="Learn Typescript with React"
+        description="Deep dive in Typescript with react"
+      />
+    </main>
+  );
+}
