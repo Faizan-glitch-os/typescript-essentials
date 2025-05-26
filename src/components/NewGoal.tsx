143d1d8e9fdc0ddb0b27df50f4c46ec5b7e8fbef
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-26 10:48:53 +0500

    new goals can be added now using refs

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,19 +1,28 @@
-import { type FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 import { GoalsType } from "../App";
 
 type NewGoalProps = {
   onAddGoal: (newGoal: GoalsType) => void;
 };
 export default function NewGoal({ onAddGoal }: NewGoalProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLInputElement>(null);
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const formData = new FormData(event.currentTarget);
+    // const formData = new FormData(event.currentTarget);
 
-    const newGoal: GoalsType = {
+    /* const newGoal: GoalsType = {
       id: Math.random(),
       title: formData.get("title") as string,
       description: formData.get("description") as string,
+    }; */
+
+    const newGoal: GoalsType = {
+      id: Math.random(),
+      title: titleRef.current!.value,
+      description: descriptionRef.current!.value,
     };
 
     onAddGoal(newGoal);
@@ -23,11 +32,16 @@ export default function NewGoal({ onAddGoal }: NewGoalProps) {
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="title">Goal Title</label>
-        <input type="text" id="title" name="title" />
+        <input ref={titleRef} type="text" id="title" name="title" />
       </p>
       <p>
         <label htmlFor="description">Goal Description</label>
-        <input type="text" id="description" name="description" />
+        <input
+          ref={descriptionRef}
+          type="text"
+          id="description"
+          name="description"
+        />
       </p>
       <p>
         <button>Add Goal</button>
