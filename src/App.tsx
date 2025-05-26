fbc915559edcc83cfbca781a42d2ef52e629879a
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-26 09:32:46 +0500

    Add Goal button now add goals to the list, but goals are hard coded

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,36 @@ import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 
 import GoldImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+type GoalsType = { id: string | number; title: string; description: string }[];
 
 export default function App() {
+  const [goals, setGoals] = useState<GoalsType>([]);
+
+  function handleAddGoal() {
+    setGoals((previousGoals) => {
+      const newGoal = {
+        id: "g-1",
+        title: "Learn React + Typescript",
+        description: "By doing real projects",
+      };
+      return [...previousGoals, newGoal];
+    });
+  }
   return (
     <main>
       <Header img={{ src: GoldImg, alt: "image of goal" }}>
         <h1>Your first goal</h1>
       </Header>
-      <CourseGoal
-        title="Learn Typescript with React"
-        description="Deep dive in Typescript with react"
-      />
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li>
+            <CourseGoal title={goal.title} description={goal.description} />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
